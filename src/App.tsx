331d27bf6f0a2a34fc8367327b9d235c6f9b7457
mistople <ddossett@microsoft.com>
2021-02-13 17:55:23 -0800

    Setup main layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
+
 function App() {
+  const [isSidebarVisible, setIsSidebarVisible] = useState(false);
+  
+  function handleClick() {
+    setIsSidebarVisible(!isSidebarVisible)
+  }
+
   return (
-    <div>
-      <p>Foo</p>
+    <div className={'app'}>
+      <header className={'header'}>
+        <h1>Grid Playground</h1>
+        <button onClick={handleClick}>View code</button>
+      </header>
+      <main className={'main'}>
+      </main>
+      {isSidebarVisible && <div className={'sidebar'} />}
     </div>
   );
 }
