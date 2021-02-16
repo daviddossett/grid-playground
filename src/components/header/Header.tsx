36b5a5baeba6dd9a409b572c822054dbf713779c
mistople <ddossett@microsoft.com>
2021-02-15 20:11:24 -0800

    Group input sections

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -19,47 +19,49 @@ export const Header = (props: HeaderProps) => {
 	return (
 		<header className={'header'}>
 			<h1>Grid Playground</h1>
-			<div className={'input-group'}>
-				<TextInput
-					type={'number'}
-					label={'Column count'}
-					value={props.gridState.columnCount}
-					onChange={props.onColumnCountChange}
-				/>
-				<TextInput
-					type={'number'}
-					label={'Column gap'}
-					value={props.gridState.columnGap}
-					onChange={props.onColumnGapChange}
-				/>
-			</div>
-			<div className={'input-group'}>
-				<TextInput
-					type={'number'}
-					label={'Row count'}
-					value={props.gridState.rowCount}
-					onChange={props.onRowCountChange}
-				/>
-				<TextInput
-					type={'number'}
-					label={'Row gap'}
-					value={props.gridState.rowGap}
-					onChange={props.onRowGapChange}
-				/>
-			</div>
-			<div className={'input-group'}>
-				<TextInput
-					type={'number'}
-					label={'Padding Top and Bottom'}
-					value={props.gridState.paddingTopBottom}
-					onChange={props.onPaddingTopBottomChange}
-				/>
-				<TextInput
-					type={'number'}
-					label={'Padding Left and Right'}
-					value={props.gridState.paddingLeftRight}
-					onChange={props.onPaddingLeftRightChange}
-				/>
+			<div className={'input-groups-wrapper'}>
+				<div className={'input-group'}>
+					<TextInput
+						type={'number'}
+						label={'Column count'}
+						value={props.gridState.columnCount}
+						onChange={props.onColumnCountChange}
+					/>
+					<TextInput
+						type={'number'}
+						label={'Column gap'}
+						value={props.gridState.columnGap}
+						onChange={props.onColumnGapChange}
+					/>
+				</div>
+				<div className={'input-group'}>
+					<TextInput
+						type={'number'}
+						label={'Row count'}
+						value={props.gridState.rowCount}
+						onChange={props.onRowCountChange}
+					/>
+					<TextInput
+						type={'number'}
+						label={'Row gap'}
+						value={props.gridState.rowGap}
+						onChange={props.onRowGapChange}
+					/>
+				</div>
+				<div className={'input-group'}>
+					<TextInput
+						type={'number'}
+						label={'Padding Top and Bottom'}
+						value={props.gridState.paddingTopBottom}
+						onChange={props.onPaddingTopBottomChange}
+					/>
+					<TextInput
+						type={'number'}
+						label={'Padding Left and Right'}
+						value={props.gridState.paddingLeftRight}
+						onChange={props.onPaddingLeftRightChange}
+					/>
+				</div>
 			</div>
 			<button onClick={props.onCodeButtonClick}>
 				{props.toggleButtonIcon}
