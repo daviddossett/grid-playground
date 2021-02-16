6ae3eca3192d494f3e9317997f9dbbe3133529aa
mistople <ddossett@microsoft.com>
2021-02-15 17:28:56 -0800

    Add support for rows and padding on X and Y axis

--- src/components/grid/Grid.tsx
+++ src/components/grid/Grid.tsx
@@ -1,35 +1,44 @@
-import { IColumnsState } from "../app/App"
+import { IGridState } from '../app/App';
 
 interface IGridProps {
-  columns: IColumnsState;
-  className: string;
+	className: string;
+	grid: IGridState;
 }
 
 export const Grid = (props: IGridProps) => {
-  const gridStyles = {
-    display: 'grid',
-    height: '100%',
-    gridTemplateColumns: `repeat(${props.columns.count}, 1fr)`,
-    gridColumnGap: `${props.columns.gutter}px`,
-    padding: `0 ${props.columns.margin}px`
-  }
+	const gridStyles = {
+		display: 'grid',
+		gridTemplateColumns: `repeat(${props.grid.columnCount}, 1fr)`,
+		gridColumnGap: `${props.grid.columnGap}px`,
+		padding: `${props.grid.paddingTopBottom}px ${props.grid.paddingLeftRight}px`,
+		gridTemplateRows: `repeat(${props.grid.rowCount}, 1fr)`,
+		gridRowGap: `${props.grid.rowGap}px`,
+	};
 
-  const columnBackground = { background: '#fa8686' };
+	const columnBackground = {
+		background: 'rgba(250, 134, 134, 0.3)',
+		border: '1px dashed #fa8686',
+	};
 
-  function generateColumns(columnCount: number) {
-    const columns = [];
-    for (let i = 0; i < columnCount; i++) {
-      columns.push(<div style={columnBackground} />);
-    };
-    return columns;
-  }
+	function generateColumns(rowCount: number, columnCount: number) {
+		const rows = [];
 
-  const columns = generateColumns(props.columns.count);
-  console.log(columns);
+		for (let i = 0; i < rowCount; i++) {
+			const columns = [];
 
-  return (
-    <div style={gridStyles} className={props.className}>
-      {columns}
-    </div>
-  )
-}
+			for (let j = 0; j < columnCount; j++) {
+				columns.push(<div style={columnBackground} key={j} />);
+			}
+			rows.push(columns);
+		}
+		return rows;
+	}
+
+	const columns = generateColumns(props.grid.rowCount, props.grid.columnCount);
+
+	return (
+		<div style={gridStyles} className={props.className}>
+			{columns}
+		</div>
+	);
+};
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,30 +1,70 @@
-import { IColumnsState } from '../app/App';
+import { IGridState } from '../app/App';
 import { TextInput } from '../text-input/TextInput';
-import '../header/Header.css'
+import '../header/Header.css';
 
 interface HeaderProps {
-  columnsState: IColumnsState;
-  onColumnCountChange: (e: any) => void;
-  onColumnGutterChange: (e: any) => void;
-  onColumnMarginChange: (e: any) => void;
-  onCodeButtonClick: () => void;
-  toggleButtonText: string;
-  toggleButtonIcon: any;
+	gridState: IGridState;
+	onColumnCountChange: (e: any) => void;
+	onColumnGapChange: (e: any) => void;
+	onPaddingTopBottomChange: (e: any) => void;
+	onPaddingLeftRightChange: (e: any) => void;
+	onRowCountChange: (e: any) => void;
+	onRowGapChange: (e: any) => void;
+	onCodeButtonClick: () => void;
+	toggleButtonText: string;
+	toggleButtonIcon: any;
 }
 
 export const Header = (props: HeaderProps) => {
-  return (
-    <header className={'header'}>
-      <h1>Grid Playground</h1>
-      <div className={'column-inputs'}>
-        <TextInput type={'number'} label={'Count'} value={props.columnsState.count} onChange={props.onColumnCountChange} />
-        <TextInput type={'number'} label={'Gutter'} value={props.columnsState.gutter} onChange={props.onColumnGutterChange} />
-        <TextInput type={'number'} label={'Margin'} value={props.columnsState.margin} onChange={props.onColumnMarginChange} />
-      </div>
-      <button onClick={props.onCodeButtonClick}>
-        {props.toggleButtonIcon}
-        {props.toggleButtonText}
-      </button>
-    </header>
-  )
-}
+	return (
+		<header className={'header'}>
+			<h1>Grid Playground</h1>
+			<div className={'input-group'}>
+				<TextInput
+					type={'number'}
+					label={'Column count'}
+					value={props.gridState.columnCount}
+					onChange={props.onColumnCountChange}
+				/>
+				<TextInput
+					type={'number'}
+					label={'Column gap'}
+					value={props.gridState.columnGap}
+					onChange={props.onColumnGapChange}
+				/>
+			</div>
+			<div className={'input-group'}>
+				<TextInput
+					type={'number'}
+					label={'Row count'}
+					value={props.gridState.rowCount}
+					onChange={props.onRowCountChange}
+				/>
+				<TextInput
+					type={'number'}
+					label={'Row gap'}
+					value={props.gridState.rowGap}
+					onChange={props.onRowGapChange}
+				/>
+			</div>
+			<div className={'input-group'}>
+				<TextInput
+					type={'number'}
+					label={'Padding Top and Bottom'}
+					value={props.gridState.paddingTopBottom}
+					onChange={props.onPaddingTopBottomChange}
+				/>
+				<TextInput
+					type={'number'}
+					label={'Padding Left and Right'}
+					value={props.gridState.paddingLeftRight}
+					onChange={props.onPaddingLeftRightChange}
+				/>
+			</div>
+			<button onClick={props.onCodeButtonClick}>
+				{props.toggleButtonIcon}
+				{props.toggleButtonText}
+			</button>
+		</header>
+	);
+};
