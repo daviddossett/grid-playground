49161d6af7cf838ed25d73c96f35d490ea8d574d
mistople <ddossett@microsoft.com>
2021-02-15 21:30:51 -0800

    Added icons to inputs

--- src/components/grid/Grid.tsx
+++ src/components/grid/Grid.tsx
@@ -20,6 +20,7 @@ export const Grid = (props: IGridProps) => {
 	const columnBackground = {
 		background: 'rgba(250, 134, 134, 0.3)',
 		border: '1px dashed #fa8686',
+		borderRadius: '2px',
 	};
 
 	function generateColumns(rowCount: number, columnCount: number) {
--- src/components/text-input/TextInput.tsx
+++ src/components/text-input/TextInput.tsx
@@ -1,23 +1,33 @@
 import '../text-input/TextInput.css';
 
 interface TextInputProps {
-  icon?: string;
-  type: string
-  value?: string | number;
-  placeholder?: string;
-  label: string;
-  onChange?: (e: any) => void;
+	icon?: any;
+	type: string;
+	value?: string | number;
+	placeholder?: string;
+	label: string;
+	onChange?: (e: any) => void;
 }
 
 function handleFocus(e: any) {
-  e.target.select();
+	e.target.select();
 }
 
 export const TextInput = (props: TextInputProps) => {
-  return (
-    <>
-      <label aria-label={props.label} htmlFor={props.label}>{props.label}</label>
-      <input type={props.type} id={props.label} value={props.value} onFocus={handleFocus} onChange={props.onChange} />
-    </>
-  )
-}
+	return (
+		<div className={'input-with-icon'}>
+			<label aria-label={props.label} htmlFor={props.label}>
+				{props.label}
+			</label>
+			{props.icon}
+			<input
+				className={'input'}
+				type={props.type}
+				id={props.label}
+				value={props.value}
+				onFocus={handleFocus}
+				onChange={props.onChange}
+			/>
+		</div>
+	);
+};
