da5d2982f1207f62e2163034768ecf2c7cf4059d
mistople <ddossett@microsoft.com>
2021-02-21 11:42:03 -0800

    Add support for sidebar sections

---
+++ src/components/editor-section/EditorSection.tsx
@@ -0,0 +1,9 @@
+import '../editor-section/EditorSection.css';
+
+interface EditorSectionProps {
+	children: JSX.Element;
+}
+
+export const EditorSection = ({ children }: EditorSectionProps) => {
+	return <section className={'section-container'}>{children}</section>;
+};
--- src/components/editor-sidebar/EditorSidebar.tsx
+++ src/components/editor-sidebar/EditorSidebar.tsx
@@ -8,6 +8,7 @@ import {
 	paddingLeftRight,
 } from '../../icons/icons';
 import { IGridState } from '../app/App';
+import { EditorSection } from '../editor-section/EditorSection';
 import { TextInput } from '../text-input/TextInput';
 import './EditorSidebar.css';
 
@@ -26,56 +27,10 @@ export const EditorSidebar = ({
 }: EditorSidebarProps) => {
 	return (
 		<div className={`${className} editor-sidebar`}>
-			<h1>Grid Playground</h1>
-			<TextInput
-				type={'number'}
-				name={'columnCount'}
-				label={'Column count'}
-				value={gridState.columnCount}
-				onChange={onInputChange}
-				icon={columnCount}
-			/>
-			<TextInput
-				type={'number'}
-				name={'columnGap'}
-				label={'Column gap'}
-				value={gridState.columnGap}
-				onChange={onInputChange}
-				icon={columnGap}
-			/>
-			<TextInput
-				type={'number'}
-				name={'rowCount'}
-				label={'Row count'}
-				value={gridState.rowCount}
-				onChange={onInputChange}
-				icon={rowCount}
-			/>
-			<TextInput
-				type={'number'}
-				name={'rowGap'}
-				label={'Row gap'}
-				value={gridState.rowGap}
-				onChange={onInputChange}
-				icon={rowGap}
-			/>
-
-			<TextInput
-				type={'number'}
-				name={'paddingTopBottom'}
-				label={'Padding Top and Bottom'}
-				value={gridState.paddingTopBottom}
-				onChange={onInputChange}
-				icon={paddingTopBottom}
-			/>
-			<TextInput
-				type={'number'}
-				name={'paddingLeftRight'}
-				label={'Padding Left and Right'}
-				value={gridState.paddingLeftRight}
-				onChange={onInputChange}
-				icon={paddingLeftRight}
-			/>
+			<h1 className={'app-title'}>Grid Playground</h1>
+			<EditorSection>
+				<h2>Columns</h2>
+			</EditorSection>
 		</div>
 	);
 };
