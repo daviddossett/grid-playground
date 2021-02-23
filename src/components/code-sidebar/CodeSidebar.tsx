3fedcfaec9eef33829648df21fe703ac45057060
mistople <ddossett@microsoft.com>
2021-02-22 22:20:51 -0800

    Added support for copy to clipboard

--- src/components/code-sidebar/CodeSidebar.tsx
+++ src/components/code-sidebar/CodeSidebar.tsx
@@ -1,53 +1,56 @@
-import React, { useLayoutEffect } from 'react';
+import React, { useLayoutEffect, useState } from 'react';
 import { IGridState } from '../app/App';
 import './CodeSidebar.css';
 import Prism from 'prismjs';
 import '../../styles/prism.css';
-import { closeIcon } from '../../icons/icons';
 
 interface SidebarProps {
 	gridState: IGridState;
-	onClose?: () => void;
 	className?: string;
 }
 
-export const CodeSidebar = ({
-	gridState,
-	onClose,
-	className,
-}: SidebarProps) => {
+export const CodeSidebar = ({ gridState, className }: SidebarProps) => {
+	const [buttonText, setButtonText] = useState('Copy');
 	useLayoutEffect(() => {
 		Prism.highlightAll();
 	});
 
+	const codeSnippet = `.grid-container { 
+  grid-template-columns: repeat(${gridState.columnCount}, 1fr);
+  grid-column-gap: ${gridState.columnGap}px;
+  grid-template-rows: repeat(${gridState.rowCount}, 1fr);
+  grid-row-gap: ${gridState.rowGap}px;
+  padding: ${gridState.paddingTopBottom}px ${gridState.paddingLeftRight}px; 
+}`;
+
+	function updateButtonText() {
+		setButtonText('Copied');
+		setTimeout(() => {
+			setButtonText('Copy');
+		}, 1500);
+	}
+
 	function handleCodeCopyClick() {
-		console.log('Copied the code');
+		updateButtonText();
+		const el = document.createElement('textarea');
+		el.value = codeSnippet;
+		document.body.appendChild(el);
+		el.select();
+		document.execCommand('copy');
+		document.body.removeChild(el);
 	}
 
 	return (
 		<div className={`${className} code-sidebar`}>
 			<header className={'code-sidebar-header'}>
-				<h2>Code</h2>
+				<h2>CSS</h2>
+				<button className={'copy-code-button'} onClick={handleCodeCopyClick}>
+					{buttonText}
+				</button>
 			</header>
 			<div className={'code-sidebar-content'}>
 				<pre>
-					<code className={'language-css'}>{`/* CSS */`}</code>
-					<br />
-					<code className={'language-css'}>
-						{`grid-template-columns: repeat(${gridState.columnCount}, 1fr);`}
-					</code>
-					<code className={'language-css'}>
-						{`grid-column-gap: ${gridState.columnGap}px;`}
-					</code>
-					<code className={'language-css'}>
-						{`grid-template-rows: repeat(${gridState.rowCount}, 1fr);`}
-					</code>
-					<code className={'language-css'}>
-						{`grid-row-gap: ${gridState.rowGap}px;`}
-					</code>
-					<code className={'language-css'}>
-						{`padding: ${gridState.paddingTopBottom}px ${gridState.paddingLeftRight}px;`}
-					</code>
+					<code className={'language-css'}>{codeSnippet}</code>
 				</pre>
 			</div>
 		</div>
