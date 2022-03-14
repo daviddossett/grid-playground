148ae0cea56304e69f5be77afbe7e9a91681ebae
David Dossett <ddossett@microsoft.com>
2022-03-14 15:17:51 -0700

    Update

--- src/components/gap-editor/GapEditor.tsx
+++ src/components/gap-editor/GapEditor.tsx
@@ -10,7 +10,7 @@ interface GapEditorProps {
   gridState: IGridState;
 }
 
-export const GridGapEditor = ({ onGapChange, gridState }: GapEditorProps) => {
+export const GapEditor = ({ onGapChange, gridState }: GapEditorProps) => {
   return (
     <EditorSection>
       <h2>Gap</h2>
